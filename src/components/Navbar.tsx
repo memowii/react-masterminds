6d01acb5ba5d99a9f2666db2165e15c434e20fbb
memowii <memogl25@gmail.com>
2020-10-08 19:18:45 -0500

    Delete unsed files

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGamepad } from "@fortawesome/free-solid-svg-icons";
 
-import "./Navbar.css";
-
 export const Navbar = () => (
   <nav className="navbar navbar-dark bg-dark py-3">
     <div className="container">
       <div className="row m-auto">
         <FontAwesomeIcon icon={faGamepad} size="2x" className="text-white" />
-        <div className="h1 ml-3 my-auto text-light">React Masterminds</div>
+        <div className="h4 ml-3 my-auto text-light">React Masterminds</div>
       </div>
     </div>
   </nav>
