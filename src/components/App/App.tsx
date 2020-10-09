cc5b56759d123ac5906a90f3737a4aeb5f33e3de
memowii <memogl25@gmail.com>
2020-10-08 19:12:26 -0500

    Add Navbar

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Layout } from "../Layout";
+
+export const App = () => {
+  return (
+    <Layout>
+      <div>App</div>
+    </Layout>
+  )
+};
+
+export default App;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Navbar } from "./Navbar";
+
+interface LayoutProps {
+  children: JSX.Element;
+}
+
+export const Layout = (props: LayoutProps) => (
+  <React.Fragment>
+    <Navbar />
+    {props.children}
+  </React.Fragment>
+);
