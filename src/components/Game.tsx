73bd11cf3bae45b36221b5ca0198604728459336
memowii <memogl25@gmail.com>
2020-10-11 21:00:19 -0500

    When toggle change winnerGuess

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
-import './Game.css'
+import "./Game.css";
+import { getRandomInt } from "../utils";
+
+enum GameDifficulty {
+  Easy = "EASY",
+  Medium = "MEDIUM",
+  Hard = "HARD",
+}
 
 export const Mastermind = () => {
+  const [guess, setGuess] = useState<number>(0);
+  const [winnerGuess, setWinnerGuess] = useState<number>(generateWinnerGuess());
+
   const toggleActive = (e: React.MouseEvent): void => {
     e.preventDefault();
 
@@ -20,24 +30,68 @@ export const Mastermind = () => {
     anchorsArr.map((anchor) => anchor.classList.remove("active"));
 
     target.classList.add("active");
+
+    console.log("target.dataset.difficulty", target.dataset.difficulty);
+
+    setWinnerGuess(generateWinnerGuess(target.dataset.difficulty as string));
   };
+
+  function generateWinnerGuess(
+    difficulty: string = GameDifficulty.Easy
+  ): number {
+    let winnerGuess: number;
+
+    switch (difficulty) {
+      case GameDifficulty.Easy:
+        console.log("easy");
+        winnerGuess = getRandomInt(0, 10);
+        break;
+      case GameDifficulty.Medium:
+        console.log("med");
+        winnerGuess = getRandomInt(0, 100);
+        break;
+      case GameDifficulty.Hard:
+        console.log("hard");
+        winnerGuess = getRandomInt(0, 1000);
+        break;
+      default:
+        winnerGuess = 0;
+    }
+
+    return winnerGuess;
+  }
 
   return (
     <>
       <div className="form-group">
         <ul className="nav nav-pills nav-fill">
           <li className="nav-item">
-            <a href="/" className="nav-link active" onClick={toggleActive}>
+            <a
+              href="/"
+              className="nav-link active"
+              data-difficulty={GameDifficulty.Easy}
+              onClick={toggleActive}
+            >
               EASY
             </a>
           </li>
           <li className="nav-item">
-            <a href="/" className="nav-link" onClick={toggleActive}>
+            <a
+              href="/"
+              className="nav-link"
+              data-difficulty={GameDifficulty.Medium}
+              onClick={toggleActive}
+            >
               MEDIUM
             </a>
           </li>
           <li className="nav-item">
-            <a href="/" className="nav-link" onClick={toggleActive}>
+            <a
+              href="/"
+              className="nav-link"
+              data-difficulty={GameDifficulty.Hard}
+              onClick={toggleActive}
+            >
               HARD
             </a>
           </li>
@@ -48,7 +102,8 @@ export const Mastermind = () => {
           type="number"
           className="form-control game-display"
           placeholder="Enter number"
-          defaultValue="0"
+          value={guess}
+          onChange={(e) => console.log(e)}
         />
       </div>
       <div className="form-group">
